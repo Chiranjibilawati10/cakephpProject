6fb4bde8a9089c48801127eecda8e40d777a6035
Chiranjibi Lawati <chiranjibilawati08@gmail.com>
2019-09-03 15:26:56 +0545

    the new project made using cakephp

---
+++ basicApp/src/Controller/DivisionsController.php
@@ -0,0 +1,17 @@
+<?php
+    namespace App\Controller;
+    use App\Controller\AppController;
+
+    class DivisionsController extends AppController
+    {
+            public function index()
+            {
+
+            }
+
+            public function add()
+            {
+                
+            }
+    }
+?>
---
+++ basicApp/tests/Fixture/DistrictsFixture.php
@@ -0,0 +1,50 @@
+<?php
+namespace App\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+/**
+ * DistrictsFixture
+ */
+class DistrictsFixture extends TestFixture
+{
+    /**
+     * Fields
+     *
+     * @var array
+     */
+    // @codingStandardsIgnoreStart
+    public $fields = [
+        'id' => ['type' => 'integer', 'length' => 11, 'unsigned' => false, 'null' => false, 'default' => null, 'comment' => '', 'autoIncrement' => true, 'precision' => null],
+        'division_id' => ['type' => 'integer', 'length' => 11, 'unsigned' => false, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null, 'autoIncrement' => null],
+        'name' => ['type' => 'string', 'length' => 255, 'null' => false, 'default' => null, 'collate' => 'latin1_swedish_ci', 'comment' => '', 'precision' => null, 'fixed' => null],
+        'created' => ['type' => 'datetime', 'length' => null, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null],
+        'modified' => ['type' => 'datetime', 'length' => null, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null],
+        '_constraints' => [
+            'primary' => ['type' => 'primary', 'columns' => ['id'], 'length' => []],
+        ],
+        '_options' => [
+            'engine' => 'InnoDB',
+            'collation' => 'latin1_swedish_ci'
+        ],
+    ];
+    // @codingStandardsIgnoreEnd
+    /**
+     * Init method
+     *
+     * @return void
+     */
+    public function init()
+    {
+        $this->records = [
+            [
+                'id' => 1,
+                'division_id' => 1,
+                'name' => 'Lorem ipsum dolor sit amet',
+                'created' => '2019-09-03 08:45:15',
+                'modified' => '2019-09-03 08:45:15'
+            ],
+        ];
+        parent::init();
+    }
+}
